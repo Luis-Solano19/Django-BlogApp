98fcc8d621fe30dc024fcaef323a06d26bea3325
Luis Solano <luis.solano19@hotmail.com>
2024-05-29 23:48:33 -0600

    blogs app and blog model

---
+++ blogs/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from blogs.models import Blog, Category
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title', )}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured')
+    # category__category_name = from foreign key we want category_name
+    search_fields = ('id', 'title', 'category__category_name', 'status',)
+    list_editable = ('is_featured',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj,form,change):
+        obj.name = obj.name.title()
+        super().save_model(request,obj,form,change)
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Blog, BlogAdmin)
